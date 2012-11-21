1c12d98e35186f2f69d98c543c200c923c49c3be
Matt Fielding <mattfielding@cogeco.ca>
2012-11-20 20:32:56 -0500

    Added comment to bulletin

--- bulletin.php
+++ bulletin.php
@@ -1,1 +1,7 @@
+<!--
+	This file is only part of a full html page and is to be
+	included in another page (via php include, etc.) in order
+	to provide full functionality.
+-->
+
 Bulletin board posts can be posted in this area.
