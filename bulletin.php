ac853d932eb71bc48647022b88ee9ceb6feb46ba
Matt Fielding <mattfielding@cogeco.ca>
2012-11-29 20:14:44 -0500

    Added sample bulletin entries

--- bulletin.php
+++ bulletin.php
@@ -4,4 +4,49 @@
 	to provide full functionality.
 -->
 
-Bulletin board posts can be posted in this area.
+
+<ul data-role="listview" data-inset="true" name="bulletin" id="bulletin" data-theme="c">
+	<li data-role="list-divider">Announcements</li>
+	<li>
+		<a href="">
+		<h3>Blah Blah and the Blahs</h3>
+		<p><b>December 5, 2012</b></p>
+		<p style="white-space: nowrap;">This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?</p>
+		</a>
+	</li>
+	<li>
+		<a href="">
+		<h3>Tim Horton's</h3>
+		<p><b>December 9, 2012</b></p>
+		<p>Tim Horton's if offering half-price coffee for one day only!</p>
+		</a>
+	</li>
+	<li>
+		<a href="">
+		<h3>Generic Event</h3>
+		<p><b>December 10, 2012</b></p>
+		<p>This is a description on the event. This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?</p>
+		</a>
+	</li>
+	<li>
+		<a href="">
+		<h3>Muse Comes To Copp's</h3>
+		<p><b>December 12, 2012</b></p>
+		<p>Muse will be playing at Copp's Colluseum on December 12. Don't miss it!</p>
+		</a>
+	</li>
+	<li>
+		<a href="">
+		<h3>Christmas</h3>
+		<p><b>December 25, 2012</b></p>
+		<p>Merry Christmas everyone!</p>
+		</a>
+	</li>
+	<li>
+		<a href="">
+		<h3>Boxing Day Specials</h3>
+		<p><b>December 26, 2012</b></p>
+		<p>Save up to 80% at Blah Blah's Department Store! Sales are for one-day only and stock is limited, so arrive early.</p>
+		</a>
+	</li>
+</ul>
