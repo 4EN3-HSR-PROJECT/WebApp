8a7fe683728ca88e7cc65086a6d9693d5dac02a1
Matt Fielding <mattfielding@cogeco.ca>
2012-12-01 21:45:52 -0500

    Moved bulletin content into separate php file

--- bulletin.php
+++ bulletin.php
@@ -5,49 +5,7 @@
 -->
 
 
-<?php
-
-$bulletin = array
-(
-	array(
-		'title'			=>	"Blah Blah and the Blahs",
-		'link'			=>	"",
-		'date'			=>	"December 5, 2012",
-		'description'	=>	"This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?"
-	),
-	array(
-		'title'			=>	"Tim Horton's",
-		'link'			=>	"",
-		'date'			=>	"December 9, 2012",
-		'description'	=>	"Tim Horton's if offering half-price coffee for one day only!"
-	),
-	array(
-		'title'			=>	"Generic Event",
-		'link'			=>	"",
-		'date'			=>	"December 10, 2012",
-		'description'	=>	"This is a description on the event. This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?"
-	),
-	array(
-		'title'			=>	"Muse Comes To Copp's",
-		'link'			=>	"",
-		'date'			=>	"December 12, 2012",
-		'description'	=>	"Muse will be playing at Copp's Colluseum on December 12. Don't miss it!"
-	),
-	array(
-		'title'			=>	"Christmas",
-		'link'			=>	"",
-		'date'			=>	"December 25, 2012",
-		'description'	=>	"Merry Christmas everyone!"
-	),
-	array(
-		'title'			=>	"Boxing Day Specials",
-		'link'			=>	"",
-		'date'			=>	"December 26, 2012",
-		'description'	=>	"Save up to 80% at Blah Blah's Department Store! Sales are for one-day only and stock is limited, so arrive early."
-	)
-);
-
-?>
+<?php include 'bulletin/contents.php' ?>
 
 
 <ul data-role="listview" data-inset="true" name="bulletin" id="bulletin" data-theme="c">
@@ -55,11 +13,13 @@ $bulletin = array
 	<?php foreach ($bulletin as $entry) {
 		$title			= (isset($entry['title']))			? $entry['title']		: '';
 		$link			= (isset($entry['link']))			? $entry['link']		: '';
+		$image			= (isset($entry['image']))			? $entry['image']		: 'bulletin/default.png';
 		$date			= (isset($entry['date']))			? $entry['date']		: '';
 		$description	= (isset($entry['description']))	? $entry['description']	: '';
 		if (strlen($title) >= 1) {
 			echo "<li>";
 			echo "<a href=\"$link\">";
+			echo "<img src=\"$image\" />";
 			echo "<h3>$title</h3>";
 			echo "<p><b>$date</b></p>";
 			echo "<p style=\"white-space: normal;\">$description</p>";
---
+++ bulletin/contents.php
@@ -0,0 +1,41 @@
+<?php
+$bulletin = array
+(
+	array(
+		'title'			=>	"Blah Blah and the Blahs",
+		'link'			=>	"",
+		'date'			=>	"December 5, 2012",
+		'description'	=>	"This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?"
+	),
+	array(
+		'title'			=>	"Tim Horton's",
+		'link'			=>	"",
+		'date'			=>	"December 9, 2012",
+		'description'	=>	"Tim Horton's if offering half-price coffee for one day only!"
+	),
+	array(
+		'title'			=>	"Generic Event",
+		'link'			=>	"",
+		'date'			=>	"December 10, 2012",
+		'description'	=>	"This is a description on the event. This event will feature Blah Blah and the Blahs, who will be performing Blah! Does that sound fun or what?"
+	),
+	array(
+		'title'			=>	"Muse Comes To Copp's",
+		'link'			=>	"",
+		'date'			=>	"December 12, 2012",
+		'description'	=>	"Muse will be playing at Copp's Colluseum on December 12. Don't miss it!"
+	),
+	array(
+		'title'			=>	"Christmas",
+		'link'			=>	"",
+		'date'			=>	"December 25, 2012",
+		'description'	=>	"Merry Christmas everyone!"
+	),
+	array(
+		'title'			=>	"Boxing Day Specials",
+		'link'			=>	"",
+		'date'			=>	"December 26, 2012",
+		'description'	=>	"Save up to 80% at Blah Blah's Department Store! Sales are for one-day only and stock is limited, so arrive early."
+	)
+);
+?>
