285ba91cf4d73200fb8d0b2d65557190112e3e20
Matt Fielding <mattfielding@cogeco.ca>
2012-12-02 12:30:58 -0500

    Updated splash image format (cover -> contain+center)

--- splash.php
+++ splash.php
@@ -5,5 +5,5 @@
 	setTimeout ('endSplash()', 5000);
 </script>
 
-<div style="background: url(bulletin/default.png) black no-repeat scroll center top;background-size: cover;" id="splash" data-url="splash" data-role="page" data-theme="a">
+<div style="background: url(bulletin/default.png) black no-repeat scroll center center; background-size: contain;" id="splash" data-url="splash" data-role="page" data-theme="a">
 </div>
