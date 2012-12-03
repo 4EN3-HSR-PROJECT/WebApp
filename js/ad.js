8d894cee7718d8033639d842eb404a2eecaff990
Matt Fielding <mattfielding@cogeco.ca>
2012-12-03 10:35:48 -0500

    Fixed ad implementation

--- js/ad.js
+++ js/ad.js
@@ -1,9 +1,9 @@
 function random_ads()
 {
 	var ads = new Array();;
-	ads[1] = "../ads/subAd.jpg";
-	ads[2] = "../ads/timAd.jpg";
-	ads[3] = "../ads/beerStoreAd.jpg";
+	ads[1] = "ads/subAd.jpg";
+	ads[2] = "ads/timAd.jpg";
+	ads[3] = "ads/beerStoreAd.jpg";
 	var ry = Math.floor(Math.random() * ads.length) + 1;
 	document.write('<img src="' + ads[ry] + '" border=0>');
 }
