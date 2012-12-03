614b325fc0e24047544a11da17d2e71610735fbb
Matt Fielding <mattfielding@cogeco.ca>
2012-12-02 19:22:58 -0500

    Added label for bus stop; shortened splash duration to 3 seconds

--- splash.php
+++ splash.php
@@ -2,7 +2,7 @@
 	function endSplash() {
 		$.mobile.changePage ("#main", {transition: "fade"} );
 	}
-	setTimeout ('endSplash()', 5000);
+	setTimeout ('endSplash()', 3000);
 </script>
 
 <div style="background: url(splash.png) black no-repeat scroll center center; background-size: contain;" id="splash" data-url="splash" data-role="page" data-theme="a">
