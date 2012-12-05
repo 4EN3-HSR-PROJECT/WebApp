0f0004beb336f64dea4a72f3602c819841429663
Matt Fielding <mattfielding@cogeco.ca>
2012-12-05 01:10:22 -0500

    Changed splash screen behaviour

--- splash.php
+++ splash.php
@@ -2,7 +2,9 @@
 	function endSplash() {
 		$.mobile.changePage ("#main", {transition: "fade"} );
 	}
-	setTimeout ('endSplash()', 3000);
+	$(document).on("pageshow", "#splash", function () {
+		setTimeout ('endSplash()', 3000);
+	});
 </script>
 
 <div style="background: url(splash.png) black no-repeat scroll center center; background-size: contain;" id="splash" data-url="splash" data-role="page" data-theme="a">
